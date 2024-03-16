61887fb8922d8106071a829e05d5ba5a725d863f
UU Upasena <77159213+UvinUthsara@users.noreply.github.com>
2024-03-16 11:54:10 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
      <div>
       <nav className="bg-gray-800">
          <div className="container mx-auto p-2">
-          <Link to ='/'><h2 className="text-white text-2xl font-bold">React Crud</h2></Link>
+          <Link to ='/'><h2 className="text-white text-2xl font-bold">Inventory</h2></Link>
 
          </div>
       </nav>
